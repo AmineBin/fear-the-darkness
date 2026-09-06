9caaf799709b6d0c55b9be572c5423eecc365c9c
Amine <93395428+AmineBin@users.noreply.github.com>
2026-09-06 19:35:06 +0200

    Delete Scripts directory

--- Scripts/items/key.gd
+++
@@ -1,8 +0,0 @@
-extends Area3D
-
-@export var item: InvItem
-@onready var player = get_node("/root/Playground/Player") 
-
-func interact():
-	player.collect(item)
-	self.queue_free()
--- Scripts/ui/menu/sound_player.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-func audio_play(stream):
-	$AudioStreamPlayer.stream = stream
-	$AudioStreamPlayer.play()
