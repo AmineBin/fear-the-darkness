ccaac5144f9f832f42aaad0917ff406b646e6601
AmineBin <aminebin2004@gmail.com>
2026-09-26 17:24:44 +0200

    add: flashlight battery system

--- items/flashlight/flashlight.gd
+++ items/flashlight/flashlight.gd
@@ -5,9 +5,43 @@ extends Node3D
 @export var flashlight_toggle_off:AudioStreamWAV
 @export var flashlight_toggle_on:AudioStreamWAV
 
+var battery: float = 15.0
+var max_battery: float = 15.0 
+var drain_rate: float = 1.0
+
 func _ready() -> void:
 	light.visible = false
+
+func _process(delta: float) -> void:
+	if battery > 0 and light.visible == true:
+		battery -= drain_rate * delta
+		battery = max(battery, 0.0)
+		
+		var current_energy:float = 1.0
+		
+		if battery <= 3:
+			current_energy = light_dying()
+			
+		if battery <= 10:
+			light_flicker(current_energy)
+		else:
+			light.light_energy = current_energy
+		
+	else:
+		light.light_energy = 0.0 
+		
+func light_dying():
+	var ratio = battery / max_battery
+	light.light_energy = ratio * 2.0
+	return 2.0 * ratio
 	
+func light_flicker(energy:float):
+	var random_number = randi_range(1, 10)
+	if random_number == 1:
+		var flicker = randf_range(0.3, 1.0)
+		light.light_energy = energy * flicker
+	else:
+		light.light_energy = energy
 	
 func flashlight_on():
 	light.visible = true
--- props/environment/objects/radio.gd
+++ props/environment/objects/radio.gd
@@ -4,7 +4,7 @@ var is_audio_playing = false
 @onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	audio_player.play()
+	#audio_player.play()
 	is_audio_playing = true
 
 func interact(user:Node = null):
